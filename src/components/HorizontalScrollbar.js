13b328f0d251a44c36b1929099ae523b1eba378d
shubham <shuubhamsharma98@gmail.com>
2024-06-14 23:57:16 -0500

    final

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,18 +1,44 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from '@mui/material';
+import React, { useContext } from 'react';
+import ExerciseCard from './ExerciseCard';
+// import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import BodyPart from './BodyPart';
 
-const HorizontalScrollbar = ({data}) => {
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
   return (
-    <div>
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
-        {data.map((item)=>(<Box key ={item.id || item
-                                     
-        }
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
+        key={item.id || item}
         itemId={item.id || item}
-        title={item.id || item} m="0 40px"> {item}</Box>))}
-        
-    </div>
-  )
-}
+        title={item.id || item}
+        m="0 40px"
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      </Box>
+    ))}
+  </ScrollMenu>
+);
 
-export default HorizontalScrollbar
+export default HorizontalScrollbar;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,9 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions,fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
-    const SearchExercises =()=>{
+    const SearchExercises =({setExercises,bodyPart,setBodyPart})=>{
            const  [search, setSearch]= useState('');
-           const [exercises,setExercises] = useState([]);
+        //    const [exercises,setExercises] = useState([]);
            const [bodyParts, setBodyParts] = useState([]);
            useEffect(() => {
             const fetchExercisesData = async () => {
@@ -59,7 +59,7 @@ import HorizontalScrollbar from './HorizontalScrollbar';
     </Button>
    </Box>
    <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        <HorizontalScrollbar data={bodyParts} />
+   <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
       </Box>
         </Stack>
   )
